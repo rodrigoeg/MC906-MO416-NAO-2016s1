b6dc6cb403f5d50f6b00a28c8bb32e9627c2abd4
Rodrigo Goncalves <rodrigoegoncalves@gmail.com>
2016-06-19 01:43:19 -0300

    Init project

---
+++ code/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "nao.h"
+extern "C" {
+    #include "extApi.h"
+}
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int portNb=25000;
+
+	int clientID=simxStart((simxChar*)"127.0.0.1",portNb,true,true,2000,5);
+	if (clientID!=-1) {
+		cout << "Connected\n";
+
+		NAO nao;
+		nao.start();
+
+		simxFinish(clientID);
+	} else {
+		cout << "Not connected\n";
+	}
+	return(0);
+}
---
+++ code/nao.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "nao.h"
+extern "C" {
+    #include "extApi.h"
+}
+
+using namespace std;
+
+void NAO::start() {
+	cout << "NAO started\n";
+}
