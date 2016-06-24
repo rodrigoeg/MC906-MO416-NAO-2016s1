7c6b33b6a98132c0f470a42a6635b254ccd27596
Rodrigo Goncalves <rodrigoegoncalves@gmail.com>
2016-06-23 22:18:42 -0300

    Moving C++ do deprecated folder

---
+++ deprecated_code/nao.h
@@ -0,0 +1,16 @@
+#ifndef MO416_2016S1_NAO_H
+#define MO416_2016S1_NAO_H
+
+extern "C" {
+#include "extApi.h"
+}
+
+using namespace std;
+
+class NAO {
+public:
+    void start(int clientId);
+};
+
+
+#endif //MO416_2016S1_NAO_H
