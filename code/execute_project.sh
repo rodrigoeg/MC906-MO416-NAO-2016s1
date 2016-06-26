3ff0e5dfada665a5c63c74824663cd167ad9e375
Rodrigo Goncalves <rodrigoegoncalves@gmail.com>
2016-06-26 17:51:33 -0300

    Removing execute_project.sh

--- code/execute_project.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PYNAOQI_PATH=~/pynaoqi-python2.7-2.1.2.17-linux64
-
-export PYTHONPATH=${PYTHONPATH}:${PYNAOQI_PATH}
-export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${PYNAOQI_PATH}
-
-
-
-python main.py
