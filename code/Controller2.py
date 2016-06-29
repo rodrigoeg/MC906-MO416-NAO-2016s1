50e837ccc44c51e016c99932a453914e92505b3f
Giuliano Roberto Pinheiro <giulianorp2010@gmail.com>
2016-06-29 01:37:25 -0300

    Another controller with a slighly different logic and that cares about NAO's velocity

--- code/Controller2.py
+++ code/Controller2.py
@@ -10,9 +10,9 @@ from mpl_toolkits.mplot3d import Axes3D
 
 class Controller:
     # Variable intervals (greater the third parameter, better the resolution)
-    sonar_interval = np.linspace(0.25, 2.25, 51)
-    theta_interval = np.linspace(-1, 1, 51)
-    v_interval = np.linspace(0, 1, 21)
+    sonar_interval = np.linspace(0.25, 2.25, 101)
+    theta_interval = np.linspace(-1, 1, 101)
+    v_interval = np.linspace(0, 1, 101)
 
     # Internal variables (fuzzy stuff)
     lsonar = None
@@ -30,33 +30,34 @@ class Controller:
         self.v_x = ctrl.Consequent(self.v_interval, 'v_x')
 
         # we need membership functions!
-        var_names = ['low', 'mid', 'high']
-        var_names_theta = ['left', 'middle', 'right']
+        distance_levels = ['dangerous', 'unsafe', 'safe']
+        theta_directions = ['left', 'middle', 'right']
+        velocity_levels = ['slow', 'medium', 'fast']
 
-        self.lsonar.automf(names=var_names)
-        self.rsonar.automf(names=var_names)
-        self.theta.automf(names=var_names_theta)
-        self.v_x.automf(names=var_names)
+        self.lsonar.automf(names=distance_levels)
+        self.rsonar.automf(names=distance_levels)
+        self.theta.automf(names=theta_directions)
+        self.v_x.automf(names=velocity_levels)
 
-        low_mshp = mshp.trapmf(self.sonar_interval, [0.25, 0.25, 0.5, 1.0])
-        mid_mshp = mshp.gaussmf(self.sonar_interval, 1.25, 0.25)
-        high_mshp = mshp.trapmf(self.sonar_interval, [1.5, 2.0, 2.25, 2.25])
+        low_mshp = mshp.trapmf(self.sonar_interval, [0.25, 0.25, 0.3, 0.4])
+        mid_mshp = mshp.gaussmf(self.sonar_interval, 0.45, 0.15)
+        high_mshp = mshp.trapmf(self.sonar_interval, [0.5, 0.8, 2.55, 2.55])
 
-        self.lsonar['low'] = low_mshp
-        self.lsonar['mid'] = mid_mshp
-        self.lsonar['high'] = high_mshp
+        self.lsonar['dangerous'] = low_mshp
+        self.lsonar['unsafe'] = mid_mshp
+        self.lsonar['safe'] = high_mshp
 
-        self.rsonar['low'] = low_mshp
-        self.rsonar['mid'] = mid_mshp
-        self.rsonar['high'] = high_mshp
+        self.rsonar['dangerous'] = low_mshp
+        self.rsonar['unsafe'] = mid_mshp
+        self.rsonar['safe'] = high_mshp
         
-        self.theta['left'] = mshp.gaussmf(self.theta_interval, 1.0, 0.2)
-        self.theta['middle'] = mshp.gaussmf(self.theta_interval, 0, 0.6)
-        self.theta['right'] = mshp.gaussmf(self.theta_interval, -1.0, 0.2)
+        self.theta['left'] = mshp.gaussmf(self.theta_interval, 1.0, 0.45)
+        self.theta['middle'] = mshp.gaussmf(self.theta_interval, 0, 0.5)
+        self.theta['right'] = mshp.gaussmf(self.theta_interval, -1.0, 0.3)
 
-        self.v_x['low'] = mshp.gaussmf(self.v_interval, 0, 0.2)
-        self.v_x['mid'] = mshp.gaussmf(self.v_interval, 0.5, 0.3)
-        self.v_x['high'] = mshp.gaussmf(self.v_interval, 1, 0.2)
+        self.v_x['slow'] = mshp.trimf(self.v_interval, [0, 0, 0.4])
+        self.v_x['medium'] = mshp.gaussmf(self.v_interval, 0.6, 0.1)
+        self.v_x['fast'] = mshp.trapmf(self.v_interval, [0.8, 0.98, 1.0, 1.0])
 
         # Here the rules are defined!
         # look at http://pythonhosted.org/scikit-fuzzy/auto_examples/plot_control_system_advanced.html#set-up-the-fuzzy-control-system
@@ -64,33 +65,36 @@ class Controller:
         # for good examples
         rules = []
 
+        # this first rule needs a way to choose theta
         rules.append(ctrl.Rule(label="lowlow", 
-                               antecedent=(self.lsonar['low'] & self.rsonar['low']), 
-                               consequent=(self.v_x['low'])))
+                               antecedent=(self.lsonar['dangerous'] & self.rsonar['dangerous']), 
+                               consequent=(self.v_x['slow'])))
         rules.append(ctrl.Rule(label="lowmid", 
-                               antecedent=(self.lsonar['low'] & self.rsonar['mid']), 
-                               consequent=(self.theta['right'], self.v_x['mid'])))
+                               antecedent=(self.lsonar['dangerous'] & self.rsonar['unsafe']), 
+                               consequent=(self.theta['right'], self.v_x['medium'])))
         rules.append(ctrl.Rule(label="lowhigh", 
-                               antecedent=(self.lsonar['low'] & self.rsonar['high']), 
-                               consequent=(self.theta['right'], self.v_x['low'])))
+                               antecedent=(self.lsonar['dangerous'] & self.rsonar['safe']), 
+                               consequent=(self.theta['right'], self.v_x['medium'])))
+
         rules.append(ctrl.Rule(label="midlow", 
-                               antecedent=(self.lsonar['mid'] & self.rsonar['low']), 
-                               consequent=(self.theta['left'], self.v_x['mid'])))
+                               antecedent=(self.lsonar['unsafe'] & self.rsonar['dangerous']), 
+                               consequent=(self.theta['left'], self.v_x['medium'])))
         rules.append(ctrl.Rule(label="midmid", 
-                               antecedent=(self.lsonar['mid'] & self.rsonar['mid']), 
-                               consequent=(self.theta['middle'], self.v_x['mid'])))
+                               antecedent=(self.lsonar['unsafe'] & self.rsonar['unsafe']), 
+                               consequent=(self.theta['middle'], self.v_x['fast'])))
         rules.append(ctrl.Rule(label="midhigh", 
-                               antecedent=(self.lsonar['mid'] & self.rsonar['high']), 
-                               consequent=(self.theta['right'], self.v_x['high'])))
+                               antecedent=(self.lsonar['unsafe'] & self.rsonar['safe']), 
+                               consequent=(self.theta['right'], self.v_x['fast'])))
+
         rules.append(ctrl.Rule(label="highlow", 
-                               antecedent=(self.lsonar['high'] & self.rsonar['low']), 
-                               consequent=(self.theta['left'], self.v_x['low'])))
+                               antecedent=(self.lsonar['safe'] & self.rsonar['dangerous']), 
+                               consequent=(self.theta['left'], self.v_x['medium'])))
         rules.append(ctrl.Rule(label="highmid", 
-                               antecedent=(self.lsonar['high'] & self.rsonar['mid']), 
-                               consequent=(self.theta['left'], self.v_x['high'])))
+                               antecedent=(self.lsonar['safe'] & self.rsonar['unsafe']), 
+                               consequent=(self.theta['left'], self.v_x['fast'])))
         rules.append(ctrl.Rule(label="highhigh", 
-                               antecedent=(self.lsonar['high'] & self.rsonar['high']), 
-                               consequent=(self.theta['middle'], self.v_x['high'])))
+                               antecedent=(self.lsonar['safe'] & self.rsonar['safe']), 
+                               consequent=(self.theta['middle'], self.v_x['fast'])))
 
         # Create the control system
         self.system = ctrl.ControlSystem(rules=rules)
